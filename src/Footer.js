02d479158f0f06153f5f21e7f04a20e921067e3e
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2022-11-09 21:46:20 -0500

    Fix borders, padding, view in mobile

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
-import { ImageProps } from 'react-bootstrap';
+import React from 'react';
 import './Footer.css'
 
 const Footer = (props) => {
-    const imgstyle = { maxWidth: '2rem' }
-
     return (
         <footer id="footer" className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             <p className="col-md-4 mb-0 text-muted">&copy; {new Date().getFullYear()} Ratatouille Robotics Inc.</p>
 
-            <a href="https://mrsdprojects.ri.cmu.edu/2022teamb/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none" target='_blank'>
-                <img src="ratatouille-logo.png" style={imgstyle} />
+            <a href="https://mrsdprojects.ri.cmu.edu/2022teamb/" className="col-md-4 d-flex align-items-center justify-content-center mb-md-0 me-md-auto link-dark text-decoration-none" target='_blank'>
+                <img src="ratatouille-logo.png" />
             </a>
 
             <ul className="nav col-md-4 justify-content-end">
